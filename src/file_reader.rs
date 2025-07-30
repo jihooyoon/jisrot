236775e961bdd1205a06fc3eca6e815e3e6a8ba7
Ji Hoo Yoon <jihooyoon@Jis-MacBook-Air.local>
2025-07-31 01:40:33 +0700

    re-sync

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,16 +1,14 @@
-mod modal;
-mod definitions;
-use modal::*;
-use definitions::common::*;
+use crate::modal::*;
+use crate::definitions::common::*;
+use std::collections::HashMap;
 
-fn read_csv_raw(file_path: &str) -> Result<Vec<AppEvent>, Box<dyn std::error::Error>> {
+pub fn read_csv_dict(file_path: &str) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     let mut rdr = csv::Reader::from_path(file_path)?;
-    let mut records = Vec::new();
+    let headers: Vec<String> = rdr.headers()?.iter().map(|h| h.to_string()).collect();
     
-    for result in rdr.records() {
-        let record = result?;
-        records.push(record.iter().map(|s| s.to_string()).collect());
-    }
-    
-    Ok(records)
+    let mut dict_result: HashMap<String, String> = HashMap::new();
+
+    for record in rdr.records();
+
+    Ok(dict_result)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 mod modal;
+mod file_reader;
+mod definitions;
+use definitions::common::*;
 use modal::*;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+    let file_path: &str = args[1].as_str();
+
+    println!("{:?}", file_reader::read_csv_dict(file_path));
 }
